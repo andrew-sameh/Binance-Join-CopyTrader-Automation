9e603fc2085d908959747897f040255ee6af5254
Andrew Sameh <andrew.zameh@gmail.com>
2023-12-31 01:29:49 -0500

    init

---
+++ Binance_Join_CopyTrader_Automation.py
@@ -0,0 +1,192 @@
+import requests
+import time
+import json
+import simpleaudio as sa
+from typing import Optional
+
+
+# Script Default value, can be removed if the value is not changed
+# "copyModel": "FIXED_RATIO", 
+# "investAsset": "USDT", 
+# "costPerOrder": 0, 
+# "marginMode": "FOLLOW_LEAD",
+# "leverageMode": "FOLLOW_LEAD",
+# "totalStopLossRate": None,
+# "leverageValue": None,
+# "takeProfitRate": None,
+# "stopLostRate": None,
+# "maxPositionPerSymbolRate": None,
+
+#Available values for "copyModel": "FIXED_RATIO", "FIXED_AMT"
+# for fixed amount, costPerOrder is required
+# for fixed ratio, set costPerOrder to 0 (default, so it can be removed)
+#Available values for "totalStopLossRate": 0-95 % (Optional, can be removed and the value will be None)
+
+#Advanced Settings
+#Available values for "marginMode": "FOLLOW_LEAD", "CROSS", "ISOLATED"
+#Available values for "leverageMode": "FOLLOW_LEAD", "FIX_LEVERAGE"
+#Available values for "leverageValue": 1-10 X (Optional, can be removed and the value will be None), only available when "leverageMode" is "FIX_LEVERAGE"
+#Available values for "takeProfitRate": 0-2000 % ROI (Optional, can be removed and the value will be None)
+#Available values for "stopLostRate": 5-95 % ROI (Optional, can be removed and the value will be None)
+#Available values for "maxPositionPerSymbolRate": 5-95 % (Optional, can be removed and the value will be None)
+
+
+
+# List of leadPortfolioIds to check **IMPORTANT** - You need to add your the trader's leadPortfolioId in your favorite list
+# Portifolio ID can be found in the URL when you click on the trader's profile
+
+PROFILES_SETTINGS = {
+    "3745161142246130945": {
+        "leadPortfolioId": "3704029333160438785",
+        "investAmount": 100, 
+        "copyModel": "FIXED_RATIO", # can be removed since it's the default value
+        "investAsset": "USDT",  # can be removed since it's the default value
+        "costPerOrder": 0,  # can be removed since it's the default value
+        "marginMode": "FOLLOW_LEAD",  # can be removed since it's the default value
+        "leverageMode": "FOLLOW_LEAD",  # can be removed since it's the default value
+        "totalStopLossRate": None,  # can be removed since it's the default value
+        "leverageValue": None,  # can be removed since it's the default value
+        "takeProfitRate": None,  # can be removed since it's the default value
+        "stopLostRate": None,  # can be removed since it's the default value
+        "maxPositionPerSymbolRate": None, # can be removed since it's the default value
+    },
+}
+
+# Open Inspect > network tab and copy the following headers after hitting the Favorite copy traders list page
+csrftoken = ""
+bncuuid = ""
+fvideoid = ""
+fvideotoken = ""
+cookie = ""
+
+
+completed_list = []
+sound_file = "beep-07a.wav"
+headers = {
+    "authority": "www.binance.com",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "bnc-uuid": bncuuid,
+    "clienttype": "web",
+    "content-type": "application/json",
+    "csrftoken": csrftoken,
+    "fvideo-id": fvideoid,
+    "fvideo-token": fvideotoken,
+    "lang": "en",
+    "origin": "https://www.binance.com",
+    "referer": "https://www.binance.com/en/copy-trading",
+    "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "cookie": cookie,
+}
+
+
+def join_lead(
+    leadPortfolioId: str,
+    investAmount: int,
+    copyModel: str = "FIXED_RATIO",
+    costPerOrder: int = 0,
+    investAsset: str = "USDT",
+    leverageMode: str = "FOLLOW_LEAD",
+    marginMode: str = "FOLLOW_LEAD",
+    totalStopLossRate: Optional[int] = None, 
+    leverageValue: Optional[int] = None, 
+    takeProfitRate: Optional[int] = None, 
+    stopLostRate: Optional[int] = None, 
+    maxPositionPerSymbolRate: Optional[int] = None,
+):
+    url = "https://www.binance.com/bapi/futures/v1/private/future/copy-trade/copy-portfolio/create"
+    join_params = {
+        "leverageMode": leverageMode,
+        "marginMode": marginMode,
+        "investAmount": investAmount,
+        "copyModel": copyModel,
+        "costPerOrder": costPerOrder,
+        "investAsset": investAsset,
+        "leadPortfolioId": leadPortfolioId,
+        "totalStopLossRate": totalStopLossRate, # Remove it if it's not needed
+        "leverageValue": leverageValue, # Remove it if it's not needed
+        "takeProfitRate": takeProfitRate, # Remove it if it's not needed
+        "stopLostRate": stopLostRate, # Remove it if it's not needed
+        "maxPositionPerSymbolRate": maxPositionPerSymbolRate, # Remove it if it's not needed
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(join_params))
+        if response.status_code == 200 and response.json()["success"] == True:
+            r = response.json()
+            print(
+                f"Joined {leadPortfolioId} successfully!, amount: {investAmount} {investAsset}"
+            )
+            print(r.message)
+            completed_list.append(leadPortfolioId)
+            return response.json()
+        else:
+            print(f"Error joining {leadPortfolioId}!")
+            print(response.json()["message"])
+            return None
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
+
+
+def fetch_data():
+    url = "https://www.binance.com/bapi/futures/v1/friendly/future/copy-trade/home-page/query-list"
+    data = {
+        "pageNumber": 1,
+        "pageSize": 21,
+        "timeRange": "90D",
+        "dataType": "ROI",
+        "favoriteOnly": True,
+        "hideFull": False,
+        "nickname": "",
+        "order": "DESC",
+        "apiKeyOnly": False,
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
+
+
+def play_sound(file_path):
+    wave_obj = sa.WaveObject.from_wave_file(file_path)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+
+def check_portfolio(data):
+    if data and "data" in data and "list" in data["data"]:
+        for profile in data["data"]["list"]:
+            if profile["leadPortfolioId"] in PROFILES_SETTINGS and profile["leadPortfolioId"] not in completed_list:
+                if profile["currentCopyCount"] < profile["maxCopyCount"]:
+                    print(
+                        f"Alarm! {profile['nickname']} has available slots! {profile['currentCopyCount']} / {profile['maxCopyCount']} -- Go to https://www.binance.com/en/copy-trading/copy-setting?portfolioId={profile['leadPortfolioId']}"
+                    )
+                    play_sound(sound_file)
+                    join_lead(leadPortfolioId=profile["leadPortfolioId"], **PROFILES_SETTINGS[profile["leadPortfolioId"]])
+
+
+def main():
+    start_time = time.time()
+    print("Script started!")
+    while True:
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed Time: {int(elapsed_time)} seconds", end="\r")  # Print the elapsed time without a newline
+        data = fetch_data()
+        check_portfolio(data)
+        if len(completed_list) == len(PROFILES_SETTINGS):
+            print("All profiles are joined successfully!")
+            break
+        time.sleep(2)  # Wait for 2 seconds before next request
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,81 @@
+# Binance Copy-Trading Automation Script
+
+## Overview
+This Python script automates the process of joining and managing copy-trading profiles on Binance. It is designed for users who wish to follow multiple traders and automatically join their trading strategies when slots become available. The script includes features such as custom investment settings per trader, sound notifications, and automatic joining based on predefined criteria.
+
+## Features
+- **Automated Joining**: Automatically joins trading profiles based on predefined settings.
+- **Sound Notifications**: Alerts the user with a sound notification when a slot is available.
+- **Custom Investment Settings**: Allows setting individual investment parameters for each trader.
+- **Continuous Monitoring**: Runs in a loop, continuously checking for available slots.
+
+## Requirements
+- Python 3.x
+- `requests` library
+- `simpleaudio` library
+- `json` library
+- Binance account and API keys
+
+## Installation
+1. Ensure Python 3.x is installed on your system.
+2. Install the required Python libraries:
+   ```bash
+   pip install requests simpleaudio
+   ```
+3. Download the script to your local machine.
+
+## Configuration
+Before running the script, you need to configure the following:
+
+- **API Headers**: Set your Binance API headers (`csrftoken`, `bncuuid`, `fvideoid`, `fvideotoken`, `cookie`) in the script.
+- **Trader Profiles**: Define the trader profiles you want to follow in `PROFILES_SETTINGS`. Include their `leadPortfolioId` and your desired investment settings.
+- **Sound File**: Place a `.wav` sound file in the same directory as the script or update the `sound_file` variable with the path to your sound file.
+
+## Trader Profile Settings
+The script allows you to define custom settings for each trader you wish to follow. These settings are specified in the `PROFILES_SETTINGS` dictionary. Here's a breakdown of each setting:
+- `leadPortfolioId`: The unique identifier of the trader's portfolio on Binance. This is a mandatory field.
+- `investAmount`: The amount of capital you wish to allocate to copying this trader.
+- `copyModel`: Determines how the copy trading will be executed. Options are `"FIXED_RATIO"` and `"FIXED_AMT"`. `"FIXED_RATIO"` maintains a fixed ratio of your funds to the trader's moves, while `"FIXED_AMT"` uses a fixed amount for each order.
+- `costPerOrder`: Relevant only if `copyModel` is set to `"FIXED_AMT"`. It specifies the fixed amount per order.
+- `investAsset`: The asset you are investing, typically `"USDT"`.
+- `marginMode`: Can be `"FOLLOW_LEAD"`, `"CROSS"`, or `"ISOLATED"`, indicating the type of margin mode to be used.
+- `leverageMode`: Can be `"FOLLOW_LEAD"` or `"FIX_LEVERAGE"`. `"FOLLOW_LEAD"` will mimic the trader's leverage, while `"FIX_LEVERAGE"` allows you to set a fixed leverage level.
+- `totalStopLossRate`: An optional setting to specify the total stop loss rate.
+- `leverageValue`: Required if `leverageMode` is `"FIX_LEVERAGE"`. It specifies the leverage level.
+- `takeProfitRate`: An optional setting to specify the take profit rate.
+- `stopLostRate`: An optional setting to specify the stop loss rate.
+- `maxPositionPerSymbolRate`: An optional setting to limit the maximum position per symbol.
+
+## Obtaining Headers via Inspect Network Tab
+To run the script, you need to set your Binance API headers. Here's how to obtain them:
+1. **Log in to Binance**: Open your web browser, go to the Binance website, and log into your account.
+2. **Open Developer Tools**: Right-click on the webpage and select "Inspect" or use the keyboard shortcut (usually `F12` or `Ctrl+Shift+I` on Windows, `Cmd+Option+I` on Mac).
+3. **Go to the Network Tab**: In the developer tools, navigate to the "Network" tab.
+4. **Interact with the Site**: Perform an action related to copy-trading (like visiting the copy-trading page). This will generate network activity.
+5. **Find the Relevant Request**: Look for a network request that corresponds to the copy-trading action. Click on it to view details.
+6. **Copy Headers**: In the request details, find the "Headers" section. Here, you will see various headers like `csrftoken`, `bncuuid`, `fvideoid`, `fvideotoken`, and `cookie`.
+7. **Update the Script**: Copy these header values and paste them into the corresponding variables in the script.
+
+### Important Notes:
+- The headers are tied to your session and may expire, requiring you to fetch new ones periodically.
+- Be cautious with your headers and API keys. They provide access to your Binance account and should be kept secure.
+
+## Usage
+Run the script using Python:
+```bash
+python3 <script_name>.py
+```
+The script will start and continuously monitor for available slots in your favorite traders' profiles. When a slot is available, it will attempt to join using your predefined settings.
+
+## Advanced Settings
+You can customize the following advanced settings for each trader:
+- `copyModel`: "FIXED_RATIO" or "FIXED_AMT"
+- `marginMode`: "FOLLOW_LEAD", "CROSS", "ISOLATED"
+- `leverageMode`: "FOLLOW_LEAD", "FIX_LEVERAGE"
+- `totalStopLossRate`, `leverageValue`, `takeProfitRate`, `stopLostRate`, `maxPositionPerSymbolRate`: Various risk management and leverage settings.
+
+## Disclaimer
+This script is provided as-is without any guarantees or warranty. Automated trading involves significant risk. Use at your own risk.
+
+## Contributing
+The script is still in the testing phase, and contributions are welcome. If you have suggestions for improvements or bug fixes, please feel free to submit a pull request.
